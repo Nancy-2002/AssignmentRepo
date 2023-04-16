6df845dd8031c40ab8991e949ed443fda9f765a5
unknown <20cse065@gweca.ac.in>
2023-04-16 17:28:30 +0530

    Assignmet 2 which includes concepts of Inheritance and overriding

--- EmpMain.java
+++ EmpMain.java
@@ -3,13 +3,6 @@ class Emp{
 	int age;
 	int salary;
 	String designation;
-
-	Emp(String name,int age,int salary,String designation){
-		this.name=name;
-		this.age=age;
-		this.salary=salary;
-		this.designation=designation;
-	}
 	public void display(){
 		System.out.println("Employee's name:"+name);
 		System.out.println("Employee's age:"+age);
@@ -17,23 +10,62 @@ class Emp{
 		System.out.println("Employee's designation:"+designation);
 		System.out.println("--------------------------------------");
 	}
-	public void raiseSalary(int amt){
-		salary+=amt;
+	public void raiseSalary(){
+		salary+=0;
+	}
+}
+class Tester extends Emp{
+	Tester(String name,int age){
+	this.name=name;
+	if(age>=21 && age<=60){
+		this.age=age;
+		}
+	this.salary=15000;
+	this.designation="TESTER";
+	}
+	public void raiseSalary(){
+		salary+=2000;
+	}
+}
+class Programmer extends Emp{
+	Programmer(String name,int age){
+	this.name=name;
+	if(age>=21 && age<=60){
+		this.age=age;
+		}
+	this.salary=30000;
+	this.designation="PROGRAMMER";
+	}
+	public void raiseSalary(){
+		salary+=5000;
+	}
+}
+class Manager extends Emp{
+	Manager(String name,int age){
+	this.name=name;
+	if(age>=21 && age<=60){
+		this.age=age;
+		}
+	this.salary=90000;
+	this.designation="MANAGER";
+	}
+	public void raiseSalary(){
+		salary+=10000;
 	}
 }
 class EmpMain{
 	public static void main(String args[]){
-	Emp e1=new Emp("Ravi",25,30000,"Tester");
-	Emp e2=new Emp("Sujatha",35,50000,"Programmer");
-	Emp e3=new Emp("Prabhakar",45,90000,"Manager");
+	Emp e1=new Tester("Ravi",25);
+	Emp e2=new Programmer("Prashant",33);
+	Emp e3=new Manager("Mamta",45);
 
 	e1.display();
 	e2.display();
 	e3.display();
 
-	e1.raiseSalary(-5000);
-	e2.raiseSalary(7000);
-	e3.raiseSalary(10000);
+	e1.raiseSalary();
+	e2.raiseSalary();
+	e3.raiseSalary();
 
 
 	e1.display();
